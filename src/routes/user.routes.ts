26ebf94bd0a1154b399ee999127c0f9c0da257dd
Lars-Magnus Underhaug <larsmagu@gmail.com>
2020-10-01 14:49:46 +0200

    Add pagination function to backend

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import userController from '../controllers/user/';
+import { paginatedResults } from '../utils/paginations';
+import { UserModel } from '../models/user/user.model';
 
 const router = express.Router();
 
 router.post('/register', userController.register);
 router.get('/login', userController.login);
+
+// Get request to test pagination
+router.get('/users', paginatedResults(UserModel));
 
 export default router;
