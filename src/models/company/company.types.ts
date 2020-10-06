7ed5b05445e1d13d45739a99062bf0299ec181da
alexannf <alexannf@stud.ntnu.no>
2020-10-06 14:51:35 +0200

    update company schema

--- src/models/company/company.types.ts
+++ src/models/company/company.types.ts
@@ -1,4 +1,4 @@
-import { Document, Schema } from 'mongoose';
+import { Document } from 'mongoose';
 
 export interface ICompany {
   organizationNr: string;
@@ -7,9 +7,6 @@ export interface ICompany {
   phoneNr?: string;
   description?: string;
   facilities?: Array<string>;
-  componentIds?: Array<Schema.Types.ObjectId>;
-  employeeIds?: Array<Schema.Types.ObjectId>;
-  dataInstanceIds?: Array<Schema.Types.ObjectId>;
   created?: Date;
 }
 
