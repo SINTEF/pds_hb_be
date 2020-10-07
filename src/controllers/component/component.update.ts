d90851b98b9b54921fb77281f2199b16a82af2b7
Lars-Magnus Underhaug <larsmagu@gmail.com>
2020-10-07 12:39:33 +0200

    Add endpoint for updating component entries in db

--- src/controllers/component/component.update.ts
+++ src/controllers/component/component.update.ts
@@ -2,11 +2,10 @@ import express from 'express';
 import db from '../../db';
 import { ComponentModel } from '../../models';
 
-
 const update = async (req: express.Request, res: express.Response): Promise<void> => {
   db.connect();
 
-  const { obj_id ,name, size, design, revisionDate, remarks, description, data, module, equipmentGroup } = req.body;
+  const { obj_id, name, size, design, revisionDate, remarks, description, data, module, equipmentGroup } = req.body;
 
   if (!name || !data || !module || !equipmentGroup) {
     res.status(400).send({
@@ -16,7 +15,9 @@ const update = async (req: express.Request, res: express.Response): Promise<void
     return;
   }
 
-  await ComponentModel.findOneAndUpdate({_id: obj_id},{
+  await ComponentModel.findOneAndUpdate(
+    { _id: obj_id },
+    {
       name: name,
       size: size,
       design: design,
@@ -25,21 +26,23 @@ const update = async (req: express.Request, res: express.Response): Promise<void
       description: description,
       data: data,
       module: module,
-      equipmentGroup: equipmentGroup
+      equipmentGroup: equipmentGroup,
     },
-    { useFindAndModify: false })
+    { useFindAndModify: false }
+  )
     .then(() => {
       res.status(200).send({
         success: true,
-        message: 'Successfully updated the database'
-      })
-    }).catch((err) => {
-        res.status(409).send({
-            success: false,
-            message: 'Error: Values not unique',
-            duplicateField: err.keyValue
-        })
-  })
+        message: 'Successfully updated the database',
+      });
+    })
+    .catch((err) => {
+      res.status(409).send({
+        success: false,
+        message: 'Error: Values not unique',
+        duplicateField: err.keyValue,
+      });
+    });
 };
 
 //
