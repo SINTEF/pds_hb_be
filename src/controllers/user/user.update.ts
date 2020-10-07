0a42cf1f34c18f52e983901934a19c4680da3fe2
Lars-Magnus Underhaug <larsmagu@gmail.com>
2020-10-07 14:32:35 +0200

    Lint changes

--- src/controllers/user/user.update.ts
+++ src/controllers/user/user.update.ts
@@ -2,42 +2,40 @@ import express from 'express';
 import db from '../../db';
 import { UserModel } from '../../models';
 
-
-
 const update = async (req: express.Request, res: express.Response): Promise<void> => {
-    db.connect();
-  
-    const { username, email, phoneNr } = req.body;
-  
-    if (!email) {
-      res.status(400).send({
+  db.connect();
+
+  const { username, email, phoneNr } = req.body;
+
+  if (!email) {
+    res.status(400).send({
+      success: false,
+      message: 'Required field missing',
+    });
+    return;
+  }
+
+  await UserModel.findOneAndUpdate(
+    { username: username },
+    {
+      email: email,
+      phoneNo: phoneNr,
+    },
+    { useFindAndModify: false }
+  )
+    .then(() => {
+      res.status(200).send({
+        success: true,
+        message: 'Successfully updated user data',
+      });
+    })
+    .catch((err) => {
+      res.status(409).send({
         success: false,
-        message: 'Required field missing',
+        message: 'Error: Values not unique',
+        duplicateField: err.keyValue,
       });
-      return;
-    }
-  
-    await UserModel.findOneAndUpdate(
-      { username: username }, 
-      {
-        email: email,
-        phoneNo: phoneNr,
-      },
-      { useFindAndModify: false }
-    )
-      .then(() => {
-        res.status(200).send({
-          success: true,
-          message: 'Successfully updated user data',
-        });
-      })
-      .catch((err) => {
-        res.status(409).send({
-          success: false,
-          message: 'Error: Values not unique',
-          duplicateField: err.keyValue,
-        });
-      });
-  };
+    });
+};
 
 export default update;
