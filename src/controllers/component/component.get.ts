ad8016b7fd57af2477e0c6b5e1acfb7fbe114402
Martin Stiles <32123897+martinstiles@users.noreply.github.com>
2020-10-11 15:04:28 +0200

    fix/remove uneccessary id check

--- src/controllers/component/component.get.ts
+++ src/controllers/component/component.get.ts
@@ -6,15 +6,6 @@ const get = async (req: express.Request, res: express.Response): Promise<void> =
   db.connect();
   const { _id } = req.params;
 
-  // IS THE FOLLOWING OVERKILL? IF IT GETS HERE ID MUST EXIST?
-  if (!_id) {
-    res.status(400).send({
-      success: false,
-      message: 'No id received',
-    });
-    return;
-  }
-
   ComponentModel.findOne({ _id })
     .then((component) => {
       res.status(200).send({
