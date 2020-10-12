7e1d1fd44c2b288bf249c8683df4bab1467b6312
Martin Stiles <32123897+martinstiles@users.noreply.github.com>
2020-10-12 13:15:38 +0200

    fix/update schema

--- src/models/module/module.schema.ts
+++ src/models/module/module.schema.ts
@@ -4,8 +4,10 @@ export const ModuleSchema = new Schema({
   name: {
     type: String,
     required: true,
+    unique: true,
   },
   equipmentGroups: {
     type: Array,
+    required: true,
   },
 });
