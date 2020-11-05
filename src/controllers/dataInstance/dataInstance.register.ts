c9b44aa6fa0292503ceedeb788a1fee48855eed5
Martin Stiles <32123897+martinstiles@users.noreply.github.com>
2020-11-05 14:19:13 +0100

    remove sintefComment input

--- src/controllers/dataInstance/dataInstance.register.ts
+++ src/controllers/dataInstance/dataInstance.register.ts
@@ -5,7 +5,7 @@ import { DataInstanceModel } from '../../models';
 const register = (req: express.Request, res: express.Response): void => {
   db.connect();
 
-  const { company, facility, component, startDate, endDate, T, du, populationSize, failureRates, comment, sintefComment, L3 } = req.body;
+  const { company, facility, component, startDate, endDate, T, du, populationSize, failureRates, comment, L3 } = req.body;
 
   if (!company || !facility || !component || !T || !du) {
     res.status(400).send({
@@ -26,7 +26,7 @@ const register = (req: express.Request, res: express.Response): void => {
     populationSize,
     failureRates,
     comment,
-    sintefComment: sintefComment || 'No comment',
+    sintefComment: 'No comment',
     status: 'not reviewed',
     L3,
   });
