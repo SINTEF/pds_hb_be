a06f1623317f98554fc7f93bb9de55458f78e9bb
Andreas Hammer Håversen <andreas.h.haaversen@gmail.com>
2020-11-13 19:32:26 +0100

    Added update function for modules

--- src/controllers/module/index.ts
+++ src/controllers/module/index.ts
@@ -2,12 +2,14 @@ import getModulesAndEquipmentGroups from './module.getModulesAndEquipmentGroups'
 import register from './module.register';
 import addEquipmentGroup from './module.addEquipmentGroup';
 import updateEquipmentGroup from './module.updateEquipmentGroup';
+import updateModule from './module.updateModule';
 
 const moduleController = {
   getModulesAndEquipmentGroups,
   register,
   addEquipmentGroup,
   updateEquipmentGroup,
+  updateModule,
 };
 
 export default moduleController;
--- src/routes/module.routes.ts
+++ src/routes/module.routes.ts
@@ -6,6 +6,7 @@ const router = express.Router();
 router.get('/', moduleController.getModulesAndEquipmentGroups);
 router.post('/', moduleController.register);
 router.put('/', moduleController.addEquipmentGroup);
+router.put('/rename', moduleController.updateModule);
 router.put('/equipmentGroup', moduleController.updateEquipmentGroup);
 
 export default router;
